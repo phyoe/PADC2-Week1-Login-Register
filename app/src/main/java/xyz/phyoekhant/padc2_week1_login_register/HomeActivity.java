f284a5facde4248605b7e3d0bde520551a8196bd
DESKTOP-0VJV0B7\Phyoe Khant <pisces.pkz@gmail.com>
2017-06-08 21:44:22 -0700

    initialize project

---
+++ app/src/main/java/xyz/phyoekhant/padc2_week1_login_register/HomeActivity.java
@@ -0,0 +1,13 @@
+package xyz.phyoekhant.padc2_week1_login_register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
